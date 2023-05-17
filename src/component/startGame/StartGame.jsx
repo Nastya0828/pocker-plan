cc72731331fcbcdcbd5480e6cd0897df9ec76031
Анастасия <anastasiia.mikhaiilova@gmail.com>
2023-05-17 15:43:39 +0500

    Создать стартовую страницу

--- src/component/startGame/StartGame.jsx
+++ src/component/startGame/StartGame.jsx
@@ -1,11 +1,38 @@
 import React, { useState } from 'react';
 import './StartGame.css';
+import MySelect from '../select/MySelect';
 
 const StartGame = () => {
-  
+
+const [gameName, setGameName]  = useState('Название игры') 
+const setLocalStoreGameName = (e) => {
+  setGameName(e.target.value)
+  localStorage.setItem('gameName', `${gameName}`)  
+  console.log('localStorage', localStorage.getItem('gameName'))
+  }
+
+  const setLocalStoreMaplayout = (e) => {
+    console.log(e.target.value)
+  }
+
   return (
-    <div className='modal-block'>
+    <div className = 'start-block'>
+       <div className = 'modal-block'>
+        <div className = 'modal-block-name'>
+          <span> Введите название игры: </span>
+          <input className = 'modal-block__input' 
+            placeholder = 'Название'
+            type ='text' 
+            value = { gameName } 
+            onChange = {setLocalStoreGameName}
+          ></input>
+        </div>
         
+        <MySelect 
+          onChange = {setLocalStoreMaplayout}
+        ></MySelect>
+        <button className = 'modal-block__button'>Начать игру</button>
+       </div>
     </div>
   );
 }
